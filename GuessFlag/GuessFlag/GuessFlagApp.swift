0464adfcad49bb7662f60c5e8476882790beda20
Simon Zwicker <github@zwicker.io>
2024-11-05 08:59:38 +0100

    Create Intial Project for Project 2 Part 1

---
+++ GuessFlag/GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Simon Zwicker on 05.11.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
