453a63976cb27786e7b50347978991326fb3623a
Simon Zwicker <github@zwicker.io>
2024-11-07 13:44:38 +0100

    Project 2 Part 3

--- GuessFlag/GuessFlag/ContentView.swift
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var isScoreShowed: Bool = false
     @State private var scoreTitle: String = ""
+    @State private var score: Int = 0
+    @State private var questionCount: Int = 0
     
     var body: some View {
         ZStack {
@@ -27,63 +29,89 @@ struct ContentView: View {
             ])
             .ignoresSafeArea()
             
-            VStack {
-                Spacer()
-                
-                Text("Guess the flag")
-                    .font(.largeTitle.bold())
-                    .foregroundStyle(.white)
-                
-                Spacer()
-                Spacer()
-                
-                Text("Score: ???")
-                    .font(.title.bold())
-                    .foregroundStyle(.secondary)
-                
-                Spacer()
-                
-                VStack(spacing: 15.0) {
+            if questionCount == 8 {
+                VStack {
+                    Text("Dein Score: \(score)")
+                        .font(.largeTitle.bold())
+                        .foregroundStyle(.secondary)
                     
-                    VStack {
-                        Text("Tap the flag of")
-                            .font(.subheadline.weight(.heavy))
-                        
-                        Text(countries[correctAnswer])
-                            .font(.largeTitle.weight(.semibold))
+                    Button("Neu starten") {
+                        questionCount = 0
+                        score = 0
+                        isScoreShowed = false
+                        scoreTitle = ""
+                        askQuestion()
                     }
-                    .foregroundStyle(.secondary)
-                    .padding(.bottom, 20)
+                    .buttonStyle(.borderedProminent)
+                }
+            } else {
+                VStack {
+                    Spacer()
                     
-                    ForEach(0..<3) { number in
-                        Button {
-                            onFlagTapped(number)
-                        } label: {
-                            Image(countries[number].lowercased())
-                                .clipShape(.capsule)
-                                .shadow(radius: 5)
+                    Text("Guess the flag")
+                        .font(.largeTitle.bold())
+                        .foregroundStyle(.white)
+                    
+                    Spacer()
+                    Spacer()
+                    
+                    Text("Score: \(score)")
+                        .font(.title.bold())
+                        .foregroundStyle(.secondary)
+                        .contentTransition(.numericText())
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 15.0) {
+                        
+                        VStack {
+                            Text("Tap the flag of")
+                                .font(.subheadline.weight(.heavy))
+                            
+                            Text(countries[correctAnswer])
+                                .font(.largeTitle.weight(.semibold))
+                        }
+                        .foregroundStyle(.secondary)
+                        .padding(.bottom, 20)
+                        
+                        ForEach(0..<3) { number in
+                            Button {
+                                onFlagTapped(number)
+                            } label: {
+                                Image(countries[number].lowercased())
+                                    .clipShape(.capsule)
+                                    .shadow(radius: 5)
+                            }
                         }
                     }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+                    .background(.regularMaterial)
+                    .clipShape(.rect(cornerRadius: 20))
+                    .padding()
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical)
-                .background(.regularMaterial)
-                .clipShape(.rect(cornerRadius: 20))
                 .padding()
             }
-            .padding()
         }
         .alert(scoreTitle, isPresented: $isScoreShowed) {
             Button("Next Question", action: askQuestion)
         } message: {
-            Text("Your current score: ??")
+            Text("Your current score: \(score)")
         }
     }
     
     private func onFlagTapped(_ number: Int) {
         withAnimation {
-            scoreTitle = number == correctAnswer ? "Correct!" : "Wrong!"
-            isScoreShowed = true
+            let isCorrect = number == correctAnswer
+            scoreTitle = isCorrect ? "Correct!" : "Wrong!"
+            score += isCorrect ? 30: 0
+            
+            if !isCorrect {
+                scoreTitle += "\n This is the flag from \(countries[number])"
+            }
+            
+            isScoreShowed = questionCount < 7 ? true: false
+            questionCount += 1
         }
     }
     
