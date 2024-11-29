dab26790ecf4fbc65918c2fd48cf6e23ca1dbc2b
Simon Zwicker <github@zwicker.io>
2024-11-29 09:30:31 +0100

    Projekt 9 Part 1

---
+++ Navigation/Navigation/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Simon Zwicker on 29.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<1000) { index in
+                NavigationLink("Select \(index)", value: index)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                DetailView(number: selection)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    var number: Int
+    
+    var body: some View {
+        Text("Detail View \(number)")
+    }
+    
+    init(number: Int) {
+        self.number = number
+        print("Creating detail view \(number)")
+    }
+}
+
+struct Student: Hashable {
+    var id: UUID = .init()
+    var name: String
+    var age: Int
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Simon Zwicker on 29.11.24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
